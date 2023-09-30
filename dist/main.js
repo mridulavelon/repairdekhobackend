2b48ab8831adccbc4fd073a9aa98c9bb25221dea
randhirsngh057 <randhirsngh057@gmail.com>
2023-09-30 23:23:08 +0530

    new

--- dist/main.js
+++ dist/main.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule, { cors: true });
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.use((req, res, next) => {
         res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
@@ -11,8 +11,8 @@ async function bootstrap() {
         next();
     });
     app.enableCors({
-        allowedHeaders: "*",
-        origin: "*"
+        allowedHeaders: '*',
+        origin: '*',
     });
     await app.listen(8000);
 }
